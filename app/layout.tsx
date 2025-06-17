4f32ce03b0e996d69932be31f7d173703a0be6c2
Silvestre Dourado <silvestredourado20@gmail.com>
2025-06-17 12:39:42 +0000

    feat: Add Vercel Analytics integration to layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import { Analytics } from "@vercel/analytics/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,7 +21,9 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <Navbar />
-        <main>{children}</main>
+        <main>
+          {children} <Analytics />
+        </main>
         <Footer />
       </body>
     </html>
